468dda2c8c837c0bc066bf42e31fad0047a4cd18
Andre Cortez <arq.acortezg@gmail.com>
2020-12-03 19:21:24 -0300

    Desafio 2

---
+++ Desafio_2/desafio_2.js
@@ -0,0 +1,11 @@
+alert ('Para comprar bebidas alcholicas tenes que ser mayor de 18 años');
+let edad = prompt('Cuantos años tenes?');
+
+if (edad < 18) {
+    alert('Sos muy chico para comprar comprar bebidas alcoholicas');
+} else {
+     alert('Podes ingresar tu pedido en la siguiente seccion');
+}
+
+
+
